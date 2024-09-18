449dff7ca02a5289e2d35d3fa95f0c27df4c4d68
Chal1ce <752193262@qq.com>
2024-09-18 22:21:31 +0800

    增加搜索框

--- src/components/baike/baikeSearch.js
+++ src/components/baike/baikeSearch.js
@@ -1,35 +1,85 @@
 import { useState } from 'react';
-import axios from 'axios';
+import Navbar from '../ui/navbar';
+import FloatingSidebar from '../ui/FloatingSidebar';
+import styles from '../../styles/FloatingSidebar.module.css';
+import searchStyles from '../../styles/SearchBox.module.css';
 
 export default function BaikeSearch() {
-  const [query, setQuery] = useState('');
-  const [result, setResult] = useState(null);
+  const [activeSection, setActiveSection] = useState('conversation');
 
-  const handleSearch = async () => {
-    try {
-      const response = await axios.post('/api/search', { query });
-      setResult(response.data);
-    } catch (error) {
-      console.error('搜索出错:', error);
-      setResult({ error: '搜索过程中发生错误' });
-    }
+  const handleSectionChange = (section) => {
+    setActiveSection(section);
   };
 
   return (
-    <div>
-      <input
-        type="text"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        placeholder="输入搜索关键词"
-      />
-      <button onClick={handleSearch}>搜索</button>
-      {result && (
-        <div>
-          <h2>搜索结果:</h2>
-          <pre>{JSON.stringify(result, null, 2)}</pre>
-        </div>
-      )}
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <div className="flex flex-1">
+        <FloatingSidebar>
+          <button
+            className={`${styles.sidebarButton} ${activeSection === 'baikeSearch' ? styles.active : ''}`}
+            onClick={() => handleSectionChange('baikeSearch')}
+          >
+            百科搜索
+            <svg viewBox="0 0 16 19" xmlns="http://www.w3.org/2000/svg">
+              <path d="M7 18C7 18.5523 7.44772 19 8 19C8.55228 19 9 18.5523 9 18H7ZM8.70711 0.292893C8.31658 -0.0976311 7.68342 -0.0976311 7.29289 0.292893L0.928932 6.65685C0.538408 7.04738 0.538408 7.68054 0.928932 8.07107C1.31946 8.46159 1.95262 8.46159 2.34315 8.07107L8 2.41421L13.6569 8.07107C14.0474 8.46159 14.6805 8.46159 15.0711 8.07107C15.4616 7.68054 15.4616 7.04738 15.0711 6.65685L8.70711 0.292893ZM9 18L9 1H7L7 18H9Z" />
+            </svg>
+          </button>
+          <button
+            className={`${styles.sidebarButton} ${activeSection === 'conversation' ? styles.active : ''}`}
+            onClick={() => handleSectionChange('conversation')}
+          >
+            物种知识问答
+            <svg viewBox="0 0 16 19" xmlns="http://www.w3.org/2000/svg">
+              <path d="M7 18C7 18.5523 7.44772 19 8 19C8.55228 19 9 18.5523 9 18H7ZM8.70711 0.292893C8.31658 -0.0976311 7.68342 -0.0976311 7.29289 0.292893L0.928932 6.65685C0.538408 7.04738 0.538408 7.68054 0.928932 8.07107C1.31946 8.46159 1.95262 8.46159 2.34315 8.07107L8 2.41421L13.6569 8.07107C14.0474 8.46159 14.6805 8.46159 15.0711 8.07107C15.4616 7.68054 15.4616 7.04738 15.0711 6.65685L8.70711 0.292893ZM9 18L9 1H7L7 18H9Z" />
+            </svg>
+          </button>
+          <button
+            className={`${styles.sidebarButton} ${activeSection === 'recognition' ? styles.active : ''}`}
+            onClick={() => handleSectionChange('recognition')}
+          >
+            物种识别
+            <svg viewBox="0 0 16 19" xmlns="http://www.w3.org/2000/svg">
+              <path d="M7 18C7 18.5523 7.44772 19 8 19C8.55228 19 9 18.5523 9 18H7ZM8.70711 0.292893C8.31658 -0.0976311 7.68342 -0.0976311 7.29289 0.292893L0.928932 6.65685C0.538408 7.04738 0.538408 7.68054 0.928932 8.07107C1.31946 8.46159 1.95262 8.46159 2.34315 8.07107L8 2.41421L13.6569 8.07107C14.0474 8.46159 14.6805 8.46159 15.0711 8.07107C15.4616 7.68054 15.4616 7.04738 15.0711 6.65685L8.70711 0.292893ZM9 18L9 1H7L7 18H9Z" />
+            </svg>
+          </button>
+        </FloatingSidebar>
+        <main className="flex-1">
+          {activeSection === 'baikeSearch' && (
+            <div className={searchStyles.searchContainer}>
+              <h1 className={searchStyles.searchTitle}>百科搜索</h1>
+              <p className={searchStyles.searchSubtitle}>输入物种名称，探索知识的海洋</p>
+              <div className={searchStyles.searchInputContainer}>
+                <input
+                  type="text"
+                  className={searchStyles.searchInput}
+                  placeholder="请输入搜索内容"
+                />
+                <button className={searchStyles.searchButton}>
+                  搜索
+                </button>
+              </div>
+            </div>
+          )}
+        </main>
+        <main className="flex-1">
+          {activeSection === 'conversation' && (
+            <div className={searchStyles.searchContainer}>
+              <p className={searchStyles.searchSubtitle}>请输入您的问题，我将为您解答</p>
+              <div className={searchStyles.searchInputContainer}>
+                <input
+                  type="text"
+                  className={searchStyles.searchInput}
+                  placeholder="请输入搜索内容"
+                />
+                <button className={searchStyles.searchButton}>
+                  发送
+                </button>
+              </div>
+            </div>
+          )}
+        </main>
+      </div>
     </div>
   );
 }
