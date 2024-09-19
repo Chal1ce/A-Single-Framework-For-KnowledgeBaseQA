45ba606609cfcf2e34c2ef4c6f5c8547e14d9ab0
Chal1ce <752193262@qq.com>
2024-09-19 16:01:04 +0800

    添加隐藏搜索框功能

--- src/components/ui/FloatingSidebar.js
+++ src/components/ui/FloatingSidebar.js
@@ -3,6 +3,7 @@ import styles from '../../styles/FloatingSidebar.module.css';
 
 const FloatingSidebar = ({ children }) => {
   const [isSettingsOpen, setIsSettingsOpen] = useState(false);
+  const [isSidebarVisible, setIsSidebarVisible] = useState(true);
   const [baseUrl, setBaseUrl] = useState('');
   const [apiKey, setApiKey] = useState('');
   const [modelName, setModelName] = useState('');
@@ -14,6 +15,10 @@ const FloatingSidebar = ({ children }) => {
     setIsSettingsOpen(!isSettingsOpen);
   };
 
+  const toggleSidebar = () => {
+    setIsSidebarVisible(!isSidebarVisible);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log('设置已保存:', { baseUrl, apiKey, modelName, embedModel, needWebSearch, serperApiKey });
@@ -21,19 +26,37 @@ const FloatingSidebar = ({ children }) => {
   };
 
   return (
-    <aside className={styles.floatingSidebar}>
-      <nav className={styles.sidebarContent}>
-        {children}
-      </nav>
-      <button className={styles.button} onClick={toggleSettings}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" viewBox="0 0 20 20" height="16" fill="none" className={styles.svgIcon}>
-          <g strokeWidth="1.5" strokeLinecap="round" stroke="currentColor">
-            <circle r="2.5" cy="10" cx="10"></circle>
-            <path fillRule="evenodd" d="m8.39079 2.80235c.53842-1.51424 2.67991-1.51424 3.21831-.00001.3392.95358 1.4284 1.40477 2.3425.97027 1.4514-.68995 2.9657.82427 2.2758 2.27575-.4345.91407.0166 2.00334.9702 2.34248 1.5143.53842 1.5143 2.67996 0 3.21836-.9536.3391-1.4047 1.4284-.9702 2.3425.6899 1.4514-.8244 2.9656-2.2758 2.2757-.9141-.4345-2.0033.0167-2.3425.9703-.5384 1.5142-2.67989 1.5142-3.21831 0-.33914-.9536-1.4284-1.4048-2.34247-.9703-1.45148.6899-2.96571-.8243-2.27575-2.2757.43449-.9141-.01669-2.0034-.97028-2.3425-1.51422-.5384-1.51422-2.67994.00001-3.21836.95358-.33914 1.40476-1.42841.97027-2.34248-.68996-1.45148.82427-2.9657 2.27575-2.27575.91407.4345 2.00333-.01669 2.34247-.97026z" clipRule="evenodd"></path>
-          </g>
-        </svg>
-        <span className={styles.label}>设置</span>
-      </button>
+    <aside className={`${styles.floatingSidebar} ${isSidebarVisible ? '' : styles.hidden}`}>
+      {isSidebarVisible ? (
+        <>
+          <nav className={styles.sidebarContent}>
+            {children}
+          </nav>
+          <div className={styles.sidebarControls}>
+            <button className={`${styles.button} ${styles.toggleButton}`} onClick={toggleSidebar}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                <path d="M19 12H5M12 19l-7-7 7-7"/>
+              </svg>
+              <span className={styles.label}>隐藏</span>
+            </button>
+            <button className={styles.button} onClick={toggleSettings}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" viewBox="0 0 20 20" height="16" fill="none" className={styles.svgIcon}>
+                <g strokeWidth="1.5" strokeLinecap="round" stroke="currentColor">
+                  <circle r="2.5" cy="10" cx="10"></circle>
+                  <path fillRule="evenodd" d="m8.39079 2.80235c.53842-1.51424 2.67991-1.51424 3.21831-.00001.3392.95358 1.4284 1.40477 2.3425.97027 1.4514-.68995 2.9657.82427 2.2758 2.27575-.4345.91407.0166 2.00334.9702 2.34248 1.5143.53842 1.5143 2.67996 0 3.21836-.9536.3391-1.4047 1.4284-.9702 2.3425.6899 1.4514-.8244 2.9656-2.2758 2.2757-.9141-.4345-2.0033.0167-2.3425.9703-.5384 1.5142-2.67989 1.5142-3.21831 0-.33914-.9536-1.4284-1.4048-2.34247-.9703-1.45148.6899-2.96571-.8243-2.27575-2.2757.43449-.9141-.01669-2.0034-.97028-2.3425-1.51422-.5384-1.51422-2.67994.00001-3.21836.95358-.33914 1.40476-1.42841.97027-2.34248-.68996-1.45148.82427-2.9657 2.27575-2.27575.91407.4345 2.00333-.01669 2.34247-.97026z" clipRule="evenodd"></path>
+                </g>
+              </svg>
+              <span className={styles.label}>设置</span>
+            </button>
+          </div>
+        </>
+      ) : (
+        <button
+          className={`${styles.showButton} ${styles.exploreButton}`}
+          onClick={toggleSidebar}
+        >
+        </button>
+      )}
       {isSettingsOpen && (
         <div className={styles.settingsPanel}>
           <h3>设置</h3>
