cb5ecd24046b80233e314032ce5580e1c753d6f1
Chal1ce <752193262@qq.com>
2024-09-26 20:02:29 +0800

    导航栏改为百科问答

--- src/components/ui/navbar.js
+++ src/components/ui/navbar.js
@@ -9,7 +9,7 @@ const Navbar = ({ className, ...props }) => {
 
   const navItems = [
     { name: '首页', path: '/helloWorld' },
-    { name: '物种百科', path: '/BaikeSearch' },
+    { name: '百科问答', path: '/BaikeSearch' },
     { name: '论坛', path: '/forum' },
     { name: '关于', path: '/about' },
   ];
